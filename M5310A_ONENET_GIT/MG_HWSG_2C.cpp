71afd79124a20470248a23277caf0cef587c5ef0
tuxiaoya <178526945@QQ.com>
2020-03-26 23:42:38 +0800

    增加了CPP函数 txy 3-26 home

--- M5310A_ONENET_GIT/MG_HWSG_2C.cpp
+++ M5310A_ONENET_GIT/MG_HWSG_2C.cpp
@@ -15,3 +15,51 @@ boolean IR_Sensor_HWSG2C_Online::begin(uint8_t HWSGAddress)
 {
   GetHWSGTemp(uint8_t HWSGAddress);
 }
+
+ boolean IR_Sensor_HWSG2C_Online::Transform_HWSGUART_To_Temp(HWSG_Uart_frame huf, HWSG_T ht)
+  {
+   
+  }
+
+  void IR_Sensor_HWSG2C_Online::TXD_GETTEM_Handshake(uint8_t HWSGAddress = 0)  // 0-15+0xC0  连续发两次  命令送温度数据  CN
+  {
+      HWSG_Serial.write(HWSGAddress+0xc0);  // send 0xc0+ 2 times
+      HWSG_Serial.write(HWSGAddress+0xc0);  
+  }
+
+  void IR_Sensor_HWSG2C_Online::TXD_RESET_HWSG(uint8_t HWSGAddress = 0) // 
+  {   HWSG_Serial.write(HWSGAddress+0xF0);  // send 0xF0+0 2 times  to reset HWSG2C
+      HWSG_Serial.write(HWSGAddress+0xF0);  
+  }
+
+  void IR_Sensor_HWSG2C_Online::TXD_GETpar_Handshake(uint8_t HWSGAddress = 0) // 0-15+0xC0  连续发两次  命令HWSG送出工作参数  DN
+  {
+      HWSG_Serial.write(HWSGAddress+0xD0);  // send 0xD0+0 2 times  to  rev  parameters from  HWSG2C
+      HWSG_Serial.write(HWSGAddress+0xD0); 
+
+
+
+  }
+
+void IR_Sensor_HWSG2C_Online::TXD_SETpar_Handshake(uint8_t HWSGAddress = 0) // 0-15+0xC0  连续发两次  命令HWSG收工作参数  DN
+{
+      HWSG_Serial.write(HWSGAddress+0xE0);  // send 0xE0+0 2 times  to   send parameters to HWSG2C
+      HWSG_Serial.write(HWSGAddress+0xE0); 
+
+
+      
+
+
+
+
+}
+
+HWSGOnline_Uart_frame IR_Sensor_HWSG2C_Online::RXD__TemDataFrame(uint8_t HWSGAddress = 0) //  first is FeeddBack  HWSGAddress
+{
+
+}
+
+HWSG_Parameters_str IR_Sensor_HWSG2C_Online::RXD_Parameters(uint8_t HWSGAddress = 0) //  first is FeeddBack  HWSGAddress  16 frame  BYTE 
+{
+
+}
