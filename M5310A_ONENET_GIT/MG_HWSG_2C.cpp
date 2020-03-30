56239eebd61e441fc095ae812139444be9900ac8
tuxiaoya <178526945@QQ.com>
2020-03-31 00:20:34 +0800

    串口接收3-30

--- M5310A_ONENET_GIT/MG_HWSG_2C.cpp
+++ M5310A_ONENET_GIT/MG_HWSG_2C.cpp
@@ -47,19 +47,8 @@ boolean IR_Sensor_HWSG2C_Online::RXD_TEM_Frame(uint8_t HWSGAddress = 0) // 发出 
   if (HWSG_Serial(2).available() > 0)
   {
     // get incoming byte:
-    inByte = Serial.read();
-    // read first analog input, divide by 4 to make the range 0-255:
-    firstSensor = analogRead(A0) / 4;
-    // delay 10ms to let the ADC recover:
-    delay(10);
-    // read second analog input, divide by 4 to make the range 0-255:
-    secondSensor = analogRead(1) / 4;
-    // read switch, map it to 0 or 255L
-    thirdSensor = map(digitalRead(2), 0, 1, 0, 255);
-    // send sensor values:
-    Serial.write(firstSensor);
-    Serial.write(secondSensor);
-    Serial.write(thirdSensor);
+    inByte = HWSG_Serial.read();
+   
   }
 }
 boolean IR_Sensor_HWSG2C_Online::RXD_Parameters_HWSG(uint8_t HWSGAddress = 0) // 发出 D0+ 后 等待接受 D0+16帧byte Parameters
